5bfca8e13037c00cfb44585e0e52d63c54150451
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-03-11 16:54:10 -0600

    skele

---
+++ Shared/Potential Wells.swift
@@ -0,0 +1,12 @@
+//
+//  Potential Wells.swift
+//  Matrix Solution Schrodinger Equation
+//
+//  Created by Yoshinobu Fujikake on 3/11/22.
+//
+
+import Foundation
+
+class PotentialWells: ObservableObject {
+    
+}
