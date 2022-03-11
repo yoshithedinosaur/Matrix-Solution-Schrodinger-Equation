03e600cd344d7803abc70f08a46b6efe6d6f9586
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-03-11 13:57:44 -0600

    Initial Commit

---
+++ Shared/Matrix_Solution_Schrodinger_EquationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matrix_Solution_Schrodinger_EquationApp.swift
+//  Shared
+//
+//  Created by Yoshinobu Fujikake on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Matrix_Solution_Schrodinger_EquationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
